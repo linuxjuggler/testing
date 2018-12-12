f239ed0eddb7e51e49f09cac32ac354158ec97d3
Zaher Ghaibeh <z@zah.me>
2018-12-12 20:00:15 +0300

    more work

--- src/tests/Unit/ExampleTest.php
+++ src/tests/Unit/ExampleTest.php
@@ -3,9 +3,13 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+
+	use RefreshDatabase;
+
     /**
      * A basic test example.
      */
